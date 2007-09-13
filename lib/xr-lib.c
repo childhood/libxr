a084f121237a7621b8d3dad21031036e69b9fdbe
Ondrej Jirman <ondrej.jirman@zonio.net>
2007-09-13 22:11:30 +0200

    use g_printerr for logging

--- lib/xr-lib.c
+++ lib/xr-lib.c
@@ -4,21 +4,18 @@
 
 int xr_debug_enabled = 0;
 
-static GStaticMutex mutex = G_STATIC_MUTEX_INIT;
-
 void _xr_debug(const char* loc, const char* fmt, ...)
 {
+  va_list ap;
+  char* msg;
+
   if (fmt == NULL)
     return;
-  if (!g_thread_supported())
-    g_thread_init(NULL);
-  g_static_mutex_lock(&mutex);
-  if (loc != NULL)
-    fprintf(stderr, "%s", loc);
-  va_list ap;
+  
   va_start(ap, fmt);
-  vfprintf(stderr, fmt, ap);
+  msg = g_strdup_vprintf(fmt, ap);
   va_end(ap);
-  fprintf(stderr, "\n");
-  g_static_mutex_unlock(&mutex);
+
+  g_printerr("%s%s\n", loc ? loc : "", msg);
+  g_free(msg);
 }
