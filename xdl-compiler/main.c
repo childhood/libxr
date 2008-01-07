a31116c41a4d3c54b779deae371ad68831defe8c
Ondrej Jirman <ondrej.jirman@zonio.net>
2008-01-07 11:01:11 +0100

    relicensed to include LGPLv2 *and* LGPLv3
    This allows for easier spreading...

--- xdl-compiler/main.c
+++ xdl-compiler/main.c
@@ -1,11 +1,11 @@
 /* 
- * Copyright 2006, 2007 Ondrej Jirman <ondrej.jirman@zonio.net>
+ * Copyright 2006-2008 Ondrej Jirman <ondrej.jirman@zonio.net>
  * 
  * This file is part of libxr.
  *
  * Libxr is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option) any
+ * Software Foundation, either version 2 of the License, or (at your option) any
  * later version.
  *
  * Libxr is distributed in the hope that it will be useful, but WITHOUT ANY
